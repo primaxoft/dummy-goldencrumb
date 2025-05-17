a622b27174f8f98544cbffadc93000296cb0fb5e
Ijat Ayam <ijat191999@gmail.com>
2025-05-17 21:32:08 +0800

    feat: responsive section 4

--- app/Section4.tsx
+++ app/Section4.tsx
@@ -3,10 +3,10 @@ import { RiStarFill } from '@remixicon/react';
 export default function Section4() {
   return (
     <div className="bg-secondary-500">
-      <section className="max-container flex flex-col gap-8 py-6">
+      <section className="max-container flex flex-col gap-8 py-6 sm:py-16">
         <h2 className="text-primary-500 text-center text-6xl font-bold">What our clients say</h2>
-        <div className="flex flex-col gap-4">
-          <div className="text-primary-50 flex flex-col gap-4 rounded-3xl border p-4">
+        <div className="flex flex-col flex-wrap gap-4 sm:flex-row sm:justify-center md:gap-8">
+          <div className="text-primary-50 flex flex-col gap-4 rounded-3xl border p-4 sm:max-w-xs">
             <div className="flex items-start justify-between">
               <h4 className="text-lg font-bold">Albert Flores</h4>
               <img src="/images/person1.webp" alt="Person 1" className="h-11 w-11 rounded-full" />
@@ -24,7 +24,7 @@ export default function Section4() {
             </div>
           </div>
 
-          <div className="text-primary-50 flex flex-col gap-4 rounded-3xl border p-4">
+          <div className="text-primary-50 flex flex-col gap-4 rounded-3xl border p-4 sm:max-w-xs">
             <div className="flex items-start justify-between">
               <h4 className="text-lg font-bold">Albert Flores</h4>
               <img src="/images/person2.webp" alt="Person 2" className="h-11 w-11 rounded-full" />
@@ -42,7 +42,7 @@ export default function Section4() {
             </div>
           </div>
 
-          <div className="text-primary-50 flex flex-col gap-4 rounded-3xl border p-4">
+          <div className="text-primary-50 flex flex-col gap-4 rounded-3xl border p-4 sm:max-w-xs">
             <div className="flex items-start justify-between">
               <h4 className="text-lg font-bold">Albert Flores</h4>
               <img src="/images/person3.webp" alt="Person 3" className="h-11 w-11 rounded-full" />
